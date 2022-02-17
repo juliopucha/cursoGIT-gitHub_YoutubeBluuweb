ea14520b037986f5cfc33bcd8a0ef5ddd76d8a7c
juliopucha <juliopucha@yahoo.es>
2022-02-17 12:36:22 -0500

    funcionalidad de js y css

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hola me falto este mensaje');
+
+
+//console.log('hola me falto este mensaje');
+function Alternar(Seccion){ 
+    if (Seccion.style.display=="none"){Seccion.style.display=""}
+    else{Seccion.style.display="none"} 
+}
+//https://www.uv.es/jac/guia/estilo2.htm
